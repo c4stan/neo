e4d4c44cd8a1a3ef53bdb00028d8efced1430781
c4stan <matteobracci1@gmail.com>
2025-10-03 22:31:33 +0200

    sync

--- app/onda/public/onda.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-
-#include <std_module.h>
-
-#define viewer_app_module_name_m onda
-std_module_export_m void* onda_load ( void* );
-std_module_export_m void onda_unload ( void );
-std_module_export_m void onda_reload ( void*, void* );
